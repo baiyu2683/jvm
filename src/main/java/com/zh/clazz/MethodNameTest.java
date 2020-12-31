fc4c0bbf6acf74791d766dfc548f313ddf7a33be
zhangheng <zhangheng2683@gmail.com>
2021-01-01 00:01:53 +0800

    测试类结构

---
+++ src/main/java/com/zh/clazz/MethodNameTest.java
@@ -0,0 +1,14 @@
+package com.zh.clazz;
+
+public class MethodNameTest {
+
+    public String show(String[] a, int b) {
+        System.out.println("这是一个显示方法");
+        return "返回字符串";
+    }
+
+    public static String show(String[] a, boolean b) {
+        System.out.println("这是一个显示方法");
+        return "返回字符串";
+    }
+}
