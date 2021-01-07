b2c45be097ae44c54ed4aa1a752fcd82a38094ce
zhangheng <zhangheng2683@gmail.com>
2021-01-07 15:42:12 +0800

    类加载案例

---
+++ src/main/java/com/zh/runbytecode/DynamicProxyTest.java
@@ -0,0 +1,40 @@
+package com.zh.runbytecode;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class DynamicProxyTest {
+
+    interface IHello {
+        void sayHello();
+    }
+
+    static class Hello implements IHello {
+        @Override
+        public void sayHello() {
+            System.out.println("hello world");
+        }
+    }
+
+    static class DynamicProxy implements InvocationHandler {
+        Object originObj;
+
+        Object bind(Object originalObj) {
+            this.originObj = originalObj;
+            return Proxy.newProxyInstance(originalObj.getClass().getClassLoader(), originalObj.getClass().getInterfaces(), this);
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            System.out.println("welcome");
+            return method.invoke(originObj, args);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        IHello hello = (IHello) new DynamicProxy().bind(new Hello());
+        hello.sayHello();
+    }
+}
